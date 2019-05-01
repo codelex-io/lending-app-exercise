f6f41b2c236a43d03e3543f8073c75e8ce33215a
Sandris Artemjevs <sandris.artemjevs@wandoofinance.com>
2019-05-01 15:31:05 +0300

    status

--- src/main/groovy/io/codelex/InvalidStatusException.groovy
+++ src/main/groovy/io/codelex/InvalidStatusException.groovy
@@ -2,10 +2,17 @@ package io.codelex
 
 import org.springframework.http.HttpStatus
 
-class InvalidStatusException extends RuntimeException{
+class InvalidStatusException extends RuntimeException {
     final HttpStatus httpStatus
+    final String sessionId
 
-    InvalidStatusException(HttpStatus httpStatus) {
+    InvalidStatusException(HttpStatus httpStatus, String sessionId = null) {
         this.httpStatus = httpStatus
+        this.sessionId = sessionId
+    }
+
+    @Override
+    String getMessage() {
+        return "Invalid status code $httpStatus"
     }
 }
